cb2387e37707c0e7eb7533bc48ec0f4b306cf525
romy alula <goumiesone.dev@gmail.com>
2019-03-28 16:13:27 +0100

    Set database connection and use of asinc/await and a configuration file

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize"); // = ORM
 
-// code will go here
 
-module.exports = app;
+const env = process.env.NODE_ENV || "development";
+const config = require(path.resolve(__dirname, "config", env));
+
+const sequelize = new Sequelize(
+    config.db.database,
+    config.db.username,
+    config.db.password,
+    config.db.sequelize
+);
+
+module.exports = async  () => {
+    const app = express();
+    await sequelize.sync();
+    app.use(bodyParser.json());
+
+    // code here
+
+    return app;
+};
