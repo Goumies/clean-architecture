c097163d145ae956f71785fd20dc0ea570ab861b
romy alula <goumiesone.dev@gmail.com>
2019-03-28 17:21:11 +0100

    Implement product feature production code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Sequelize = require("sequelize"); // = ORM
 const env = process.env.NODE_ENV || "development";
 const config = require(path.resolve(__dirname, "config", env));
 
+// Application configuration
 const sequelize = new Sequelize(
     config.db.database,
     config.db.username,
@@ -13,12 +14,18 @@ const sequelize = new Sequelize(
     config.db.sequelize
 );
 
+// Application router
 module.exports = async  () => {
     const app = express();
     await sequelize.sync();
     app.use(bodyParser.json());
 
-    // code here
+    //API route to create a product
+    app.post('/products', async (request, response) => {
+        if(Object.keys(request.body).length === 0) {
+            return response.status(400).send();
+        }
+    });
 
     return app;
 };
